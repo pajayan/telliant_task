dec59c51dd13f4195ad8e258233c84b05e9d6743
Praveen17 <pajayan@goavega.com>
2020-02-14 13:27:48 +0530

    filter done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,16 @@ import { TableData } from './tabledata';
 
 @Pipe({name: 'filterPipe', pure: false})
 export class MyPipe implements PipeTransform {
-    transform(values: any, args?: any[]): any[] {
-      return values = values.filter(a => {
-        console.log('values from pipe', values);
-        return args.length ? args.indexOf(a.status) == -1 : values;
-      })      
+    transform(values: any, args?: any): any[] {
+      values = values.filter(a => {
+        return args.statusArr.indexOf(a.status) >= 0;
+      }); 
+      if(args.thValue) {
+       values = values.filter(b => {
+        return args.thValue ? b.th === args.thValue: true;
+      });  
+    }
+      return values ;  
     }
 }
 
@@ -27,64 +32,33 @@ export class AppComponent  {
     {checked:true,status:'Proposed'}, 
     {checked:true,status:'Unaccepted'}
   ];
-  filterArr = [];
-  tabledata = [];
   originalTabledata = [];
-  statusFilterArray = [];
   thArray :number[] = [];
-  thFilter = 0;
   thValue = 0;
 
+  filterObj = {
+    statusArr : [],
+    thValue: ''
+  }
+
   ngOnInit(): void {
-    this.tabledata = TableData;
     this.originalTabledata = TableData;
     for (let i = 1; i <= 32; i++) {
       this.thArray.push(i);
     }
-  }
-  updateFilter(appt) {
-    if(appt.checked) {
-      console.log(appt);
-      this.filterArr.push(appt.status);
-    }
-    else {
-      let index = this.filterArr.indexOf(appt.status)
-      this.filterArr.splice(index, 1)
-    }
-  } 
-  changed(e) {
-    const checked = e.target.checked;
-    const type = e.target.type;
-    const value  = e.target.value;
-    if(type === 'checkbox') {
-      if(checked) {
-        if(value === 'All') {
-          this.statusFilterArray = [];
-          this.thFilter = 0;
-          this.thValue = 0;
-        } else { 
-          this.statusFilterArray.push(value);
-          console.log('this.statusFilterArray', this.statusFilterArray);         
-        }
-      }else {        
-        this.statusFilterArray.splice(this.statusFilterArray.indexOf(value), 1);
-        console.log('this.statusFilterArray', this.statusFilterArray);
-        console.log('value uncheck', value);
-      }
-    }
-    else {
-      this.thFilter = value;
-    }
-    this.filterData(this.statusFilterArray, this.thFilter);
+    this.filterObj.statusArr = this.allStatuses.filter(s => s.checked).map(j => j.status);
   }
 
-  filterData(filterArray :string[], thFilter: number) {
-    this.tabledata =  this.originalTabledata.filter(item => {
-      return !filterArray.length ? true : filterArray.includes(item.status);
-    });
-    this.tabledata =  this.tabledata.filter(item => {
-      return thFilter ? item.th === thFilter :  true;
-    });
+  filterList(item) {
+    if(item.checked && item.status == "All") {
+      this.allStatuses = this.allStatuses.map(x => {
+        return { "status": x.status, "checked": true};
+      })
+    }
+    if(!item.checked && item.status != "All") {
+      this.allStatuses[0].checked = false;
+    }
+    this.filterObj.statusArr = this.allStatuses.filter(s => s.checked).map(j => j.status);
   }
 
 }
